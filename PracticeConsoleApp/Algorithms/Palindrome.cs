1519fe96d2851a8b82c47b0fb5167b652335d620
ashranhaider <ch.ashran@gmail.com>
2025-12-17 13:46:12 -0500

    Palindrome problem

---
+++ PracticeConsoleApp/Algorithms/Palindrome.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsoleApp.Algorithms
+{
+    public class Palindrome
+    {
+        public static int IsFilteredPalindrome(string code)
+        {
+            if (string.IsNullOrEmpty(code))
+                return 1;
+
+            StringBuilder filtered = new StringBuilder("");
+
+            foreach (char c in code)
+            {
+                if (Char.IsLetter(c))
+                {
+                    filtered.Append(Char.ToLower(c));
+                }
+            }
+
+            if (filtered.Length == 0)
+            {
+                return 1;
+            }
+
+            int right = filtered.Length - 1;
+
+            for(int i = 0; i < filtered.Length; i++) 
+            {
+                if (filtered[i] != filtered[right])
+                {
+                    return 0;
+                }
+
+                if(i >= right)
+                {
+                    break;
+                }
+                right--;
+            }
+            return 1;
+        }
+    }
+}
---
+++ TestProject/AlgorithmsTests/PalindromeTests.cs
@@ -0,0 +1,31 @@
+﻿using PracticeConsoleApp.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.AlgorithmsTests
+{
+    public class PalindromeTests
+    {
+        [Theory]
+        [InlineData("A1b2B!a", 1)]
+        [InlineData("abc123cba", 1)]
+        [InlineData("Z", 1)]
+        [InlineData("!!@@##", 1)]   // no letters → empty → palindrome
+        [InlineData("", 1)]
+        [InlineData("a", 1)]
+        [InlineData("ab", 0)]
+        [InlineData("No1oN", 1)]
+        [InlineData("Hello!", 0)]
+        public void IsFilteredPalindrome_ShouldReturnExpectedResult(string input, int expected)
+        {
+            // Act
+            int result = Palindrome.IsFilteredPalindrome(input);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
