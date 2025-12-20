c19aea3a2a584a2a55f9e79e86a0b3c562e4cc57
ashranhaider <ch.ashran@gmail.com>
2025-12-20 13:41:31 -0500

    NonIdenticalStringRotation problem

---
+++ PracticeConsoleApp/Algorithms/NonIdenticalStringRotation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConsoleApp.Algorithms
+{
+    public class NonIdenticalStringRotation
+    {
+        public static bool isNonTrivialRotation(string s1, string s2)
+        {
+            if (s1.Length != s2.Length)
+                return false;
+
+            if (s1 == s2)
+                return false;
+
+            string temp = s1 + s1;
+            return temp.Contains(s2);
+        }
+
+    }
+}
---
+++ TestProject/AlgorithmsTests/NonIdenticalStringRotationTests.cs
@@ -0,0 +1,82 @@
+﻿using PracticeConsoleApp.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.AlgorithmsTests
+{
+    public class NonIdenticalStringRotationTests
+    {
+        [Fact]
+        public void Returns_True_When_NonIdentical_Rotation_Exists()
+        {
+            // Arrange
+            string s1 = "abcde";
+            string s2 = "cdeab";
+
+            // Act
+            bool result = NonIdenticalStringRotation.isNonTrivialRotation(s1, s2);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Returns_False_When_Strings_Are_Identical()
+        {
+            // Arrange
+            string s1 = "abcde";
+            string s2 = "abcde";
+
+            // Act
+            bool result = NonIdenticalStringRotation.isNonTrivialRotation(s1, s2);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Returns_False_When_Not_A_Rotation()
+        {
+            // Arrange
+            string s1 = "abcde";
+            string s2 = "edcba"; // anagram but not rotation
+
+            // Act
+            bool result = NonIdenticalStringRotation.isNonTrivialRotation(s1, s2);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Returns_False_When_Lengths_Are_Different()
+        {
+            // Arrange
+            string s1 = "abcde";
+            string s2 = "abcd";
+
+            // Act
+            bool result = NonIdenticalStringRotation.isNonTrivialRotation(s1, s2);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Returns_True_For_Single_Left_Rotation()
+        {
+            // Arrange
+            string s1 = "water";
+            string s2 = "aterw";
+
+            // Act
+            bool result = NonIdenticalStringRotation.isNonTrivialRotation(s1, s2);
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
